ff453f62ad6308cd1dc75230544c60ebfd0712fa
jaspinderkaur <jaspinder.kaur028@gmail.com>
2023-12-11 13:21:42 +0400

    Set up room database for tracker module

--- tracker/tracker_data/src/main/java/com/plcoding/tracker_data/di/TrackerDataModule.kt
+++ tracker/tracker_data/src/main/java/com/plcoding/tracker_data/di/TrackerDataModule.kt
@@ -1,5 +1,8 @@
 package com.plcoding.tracker_data.di
 
+import android.app.Application
+import androidx.room.Room
+import com.plcoding.tracker_data.local.TrackerDatabase
 import com.plcoding.tracker_data.remote.OpenFoodApi
 import dagger.Module
 import dagger.Provides
@@ -36,5 +39,15 @@ object TrackerDataModule {
             .addConverterFactory(MoshiConverterFactory.create())
             .client(client)
             .build().create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTrackerDatabase(app: Application): TrackerDatabase {
+        return Room.databaseBuilder(
+            app,
+            TrackerDatabase::class.java,
+            "tracker_db",
+        ).build()
     }
 }
